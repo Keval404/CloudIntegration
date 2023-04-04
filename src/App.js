b882a8cd41ff38b8e1458dc7a18321d21df9b3b9
Akhil Sharma <akhils8493@gmail.com>
2023-04-04 23:09:49 +0530

    UPDATED FILE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 import './index.css';
+import './App.css';
 import Events from './events';
 import Logs from './logs';
 import Dropdown from './dropdown';
 import Nav from './nav';
 import Flow from './Flow';
+import PreLoader2 from './components/PreLoader1';
 
 const App = () => {
   const [showFlow, setShowFlow] = useState(false);
+
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 3200); // change 5000 to the desired duration in milliseconds
+    return () => clearTimeout(timer);
+  }, []);
+
+  const [unvisible, setUnvisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setUnvisible(true);
+    }, 3200); // change 5000 to the desired duration in milliseconds
+    return () => clearTimeout(timer);
+  }, []);
   
   return (
     <>
+    {visible && <div>
+      <div className="App">
+      <header className="App-header">
+        <PreLoader2 />
+      </header>
+    </div>
+    </div>
+    }
       {/* <div class="p-3 bg-black">
         <h1 class="p-2 px-10 mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Cloud Hive<span class="bg-blue-100 text-blue-800 text-2xl font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-2">PRO</span></span></h1>
       </div> */}
-      <Nav />
-      <Events showFlow = {showFlow} setShowFlow = {setShowFlow}/>
+      {unvisible && 
+      <div>
+        <Nav />
+        <Events showFlow = {showFlow} setShowFlow = {setShowFlow}/>
+        
+        <Flow/>
+      </div>
       
-      <Flow/>
+    }
+      
     </>
   );
 };
---
+++ src/components/PreLoader1.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+
+import * as location from "../1055-world-locations.json";
+import * as success from "../1127-success.json";
+
+const defaultOptions1 = {
+  loop: true,
+  autoplay: true,
+  animationData: location.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+const defaultOptions2 = {
+  loop: true,
+  autoplay: true,
+  animationData: success.default,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
+function PreLoader2() {
+  const [data, setData] = useState([]);
+  const [loading, setloading] = useState(undefined);
+  const [completed, setcompleted] = useState(undefined);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setloading(true);
+
+          setTimeout(() => {
+            setcompleted(true);
+          }, 1000);
+        });
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      {!completed ? (
+        <>
+          {!loading ? (
+            <Lottie options={defaultOptions1} height={200} width={200} />
+          ) : (
+            <Lottie options={defaultOptions2} height={100} width={100} />
+          )}
+        </>
+      ) : (
+        <>
+          {/* <h1>Your Data</h1> */}
+         
+        </>
+      )}
+    </>
+  );
+}
+
+export default PreLoader2;
